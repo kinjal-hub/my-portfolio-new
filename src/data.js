a0c46cf9e9f64f35a2ec2bd0010f0e97fedfc451
Kinjal Parmar <christian.kinju@gmail.com>
2026-02-17 20:15:30 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const gitHubLink = "https://github.com/kinjal-hub";
 export const infotext = "Welcome to my portfolio, I’m Kinjal Macwan, Software Developer. I have completed my graduation From Birla Vishwakarma Mahavidyalaya and I have learned React and Node.js through Udemy and a private course, and have built end-to-end projects as part of my learning.On the backend, I have developed 2–3 REST APIs using Node.js,MongoDB, and Mongoose, implementing full CRUD operations. On the frontend, I have built React components using Material UI, including filters and complete workflows. I have been coding actively which can be visible on my github profile. "
 export const email = "kinjalmac95@gmail.com";
 export const logoImg = "klogo.png"
-export const profileImg = "profile.png";
+export const profileImg = "profile.jpg";
 
 
 export const projectdetails = [
